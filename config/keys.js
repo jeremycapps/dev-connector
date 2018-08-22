734cc113e04df1924f34cab27e4c8b0018da710f
Jeremy Capps <jeremycapps@Jeremys-MacBook-Pro.local>
2018-08-21 23:23:44 -0400

    added sectreOrkey

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://jeremy:jeremy14@ds227332.mlab.com:27332/devconnector',
+  mongoURI: "mongodb://jeremy:jeremy14@ds227332.mlab.com:27332/devconnector",
+  secretOrKey: "secret"
 };

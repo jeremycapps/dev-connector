0ffab56fcd4061313fd7b61db67ac544bcd1a375
jcapps-asti <jcapps-asti@asti.com>
2018-08-24 19:26:19 -0400

    add redux form to register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 
 class Register extends Component {
   constructor() {
@@ -29,15 +31,13 @@ class Register extends Component {
       password2,
     };
 
-    // Submit user
-    axios.post('api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({ errors: err.response.data }));
+    this.props.registerUser(newUser);
   }
 
   render() {
     const { errors } = this.state;
 
+    const { user } = this.props.auth;
 
     return (
       <div className="register">
@@ -105,4 +105,13 @@ class Register extends Component {
   }
 }
 
-export default Register;
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { registerUser })(Register);

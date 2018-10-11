f420fa3226ed639c1f2cc67da1dfe0f315f274cb
Jeremy Capps <jeremycapps@Jeremys-MacBook-Pro.local>
2018-10-11 18:46:21 -0400

    add Post

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { logoutUser } from '../../actions/authActions';
-import { clearCurrentProfile } from '../../actions/profileActions';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileActions";
 class Navbar extends Component {
-
   onLogoutClick(e) {
     e.preventDefault();
     this.props.clearCurrentProfile();
@@ -18,25 +17,32 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li>
+          <Link className="nav-link" to="/feed">
+            Post Feed
+          </Link>
+        </li>
+        <li>
           <Link className="nav-link" to="/dashboard">
             Dashboard
           </Link>
         </li>
         <li className="nav-item">
-          <a href=""
-          onClick ={this.onLogoutClick.bind(this)}
-          className="nav-link">
-          <img
-            className="rounded-circle"
-            src={user.avatar}
-            alt={user.name}
-            style={{width: '25px', marginRight: '5px' }}
-            title="You must have a Gravatar connected to your email to display an image"
-          />
-          Logout
+          <a
+            href=""
+            onClick={this.onLogoutClick.bind(this)}
+            className="nav-link"
+          >
+            <img
+              className="rounded-circle"
+              src={user.avatar}
+              alt={user.name}
+              style={{ width: "25px", marginRight: "5px" }}
+              title="You must have a Gravatar connected to your email to display an image"
+            />
+            Logout
           </a>
         </li>
-    </ul>
+      </ul>
     );
 
     const guestLinks = (
@@ -51,38 +57,51 @@ class Navbar extends Component {
             Login
           </Link>
         </li>
-    </ul>
+      </ul>
     );
 
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
-          <Link className="navbar-brand" to="/">DevConnector</Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-            <span className="navbar-toggler-icon"></span>
+          <Link className="navbar-brand" to="/">
+            DevConnector
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
           </button>
 
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="/profiles"> Developers</Link>
+                <Link className="nav-link" to="/profiles">
+                  {" "}
+                  Developers
+                </Link>
               </li>
             </ul>
             {isAuthenticated ? authLinks : guestLinks}
           </div>
         </div>
       </nav>
-    )
+    );
   }
 }
 
 Navbar.propTypes = {
   logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-}
+  auth: PropTypes.object.isRequired
+};
 
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-})
+const mapStateToProps = state => ({
+  auth: state.auth
+});
 
-export default connect(mapStateToProps, { logoutUser, clearCurrentProfile })(Navbar);
+export default connect(
+  mapStateToProps,
+  { logoutUser, clearCurrentProfile }
+)(Navbar);

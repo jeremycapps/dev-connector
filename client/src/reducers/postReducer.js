d2530b75613e04b4e694ae2da72d855e502f8c76
Jeremy Capps <jeremycapps@Jeremys-MacBook-Pro.local>
2018-10-12 17:16:55 -0400

    view single post

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,5 +1,6 @@
 import {
   ADD_POST,
+  GET_POST,
   GET_POSTS,
   DELETE_POST,
   POST_LOADING
@@ -17,6 +18,12 @@ export default function(state = initialState, action) {
       return {
         ...state,
         loading: true
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: action.payload,
+        loading: false
       };
     case GET_POSTS:
       return {

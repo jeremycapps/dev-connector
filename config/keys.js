34e4f42072319515171af4066b91143fdfa11a3e
Jeremy Capps <jeremycapps@Jeremys-MacBook-Pro.local>
2018-10-12 20:29:25 -0400

    clean up w/single quotes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://jeremy:jeremy14@ds227332.mlab.com:27332/devconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
